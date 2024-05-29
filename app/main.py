787e569ee5071c4bb0e5b6fdb54f76fe6638834e
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2024-05-28 21:09:08 -0300

    echo builtin

--- app/main.py
+++ app/main.py
@@ -6,12 +6,24 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
-        if command := input().strip():
-            if command == "exit 0":
+        args = input().split()
+        if args[0] == "exit":
+            if args[1] == "0":
                 sys.exit(0)
             else:
+                command = args[0] + " " + args[1]
                 print(f"{command}: command not found")
-
+        elif args[0] == "echo":
+            command = ""
+            cont = 0
+            for x in args:
+                if cont > 0:
+                    command += x + " "
+                else:
+                    cont = 1
+            print(f"{command}")
+        else:
+            print(f"{args[0]}: command not found")
 
 if __name__ == "__main__":
     main()
